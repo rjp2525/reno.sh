7d76768117457460f5625ced1e86d01d088ab17a
rjp2525 <rjp2525@users.noreply.github.com>
2023-07-18 00:43:50 +0000

    deploy: 79893e3101ca448744ea866898b9209de21171c6

---
+++ _nuxt/index.fbbd221a.js
@@ -0,0 +1,1 @@
+import{d as e,j as t,l as n,k as o}from"./entry.f87739b3.js";const s=e({__name:"index",setup(a){return t({title:"Reno Philibert :: Software Engineer",meta:[]}),(r,i)=>(o(),n("div",null,"Something cool!"))}});export{s as default};
