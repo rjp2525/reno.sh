b741339d5d59737ce54300552d916cfe4b88f3bd
rjp2525 <renophilibert@gmail.com>
2023-07-21 02:03:48 -0600

    Change margin on larger text

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
         <div class="w-full h-full flex justify-center items-center backdrop-blur-sm bg-[#181818]/80"></div>
       </div>
       <div class="flex items-center justify-center h-full flex-col px-6 md:px-0">
-        <h1 class="m-0 md:mb-11 text-5xl leading-tight md:leading-normal md:text-[72px] text-white font-semibold md:tracking-[-0.035em] z-[114]">
+        <h1 class="m-0 md:mb-4 text-5xl leading-tight md:leading-normal md:text-[72px] text-white font-semibold md:tracking-[-0.035em] z-[114]">
           <span class="md:pr-[15px] opacity-60">Reno</span> Philibert
         </h1>
         <div class="my-2 md:m-0 text-[20px] md:leading-[20px] text-white/80 font-extralight z-[114]">
