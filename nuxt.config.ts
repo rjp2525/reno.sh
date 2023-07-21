e940f66fecb76d007a3a88d56bca99edf25d09d9
rjp2525 <renophilibert@gmail.com>
2023-07-20 22:17:50 -0600

    Try the environment variable route?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 export default defineNuxtConfig({
   app: {
-    baseURL: '',
-    buildAssetsDir: './_nuxt/',
     head: {
-      // This is not needed now that baseURL fixed the asset loading
       link: [{ rel: 'icon', type: 'image/x-icon', href: './favicon.ico'}]
     }
   },
