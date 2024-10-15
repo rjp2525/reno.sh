b90723785fe6b7ed28a9912c55411f7099167df0
rjp2525 <renophilibert@gmail.com>
2024-10-15 06:20:32 -0600

    Add AOS because lazy

--- resources/js/layouts/GuestLayout.vue
+++ resources/js/layouts/GuestLayout.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
 import { AppFooter, AppHeader } from '@/components';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    AOS.init({
+        disable: false,
+        startEvent: 'DOMContentLoaded',
+        initClassName: 'aos-init',
+        animatedClassName: 'aos-animate',
+        useClassNames: false,
+        disableMutationObserver: false,
+        debounceDelay: 50,
+        throttleDelay: 99,
+        offset: 120,
+        delay: 0,
+        duration: 400,
+        easing: 'ease',
+        once: false,
+        mirror: false,
+        anchorPlacement: 'top-bottom',
+    });
+});
 </script>
 
 <template>
-    <div>
-        <AppHeader />
+    <AppHeader />
+    <div data-aos="fade-in" :key="$page.component">
         <slot />
-        <AppFooter />
     </div>
+    <AppFooter />
 </template>
