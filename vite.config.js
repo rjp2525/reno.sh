9599f20a3fe41649c1b50eda0bef4cfaa204db19
rjp2525 <renophilibert@gmail.com>
2024-10-20 18:08:44 -0600

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
+            input: ['resources/js/app.ts', 'resources/css/screenshots.css'],
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
