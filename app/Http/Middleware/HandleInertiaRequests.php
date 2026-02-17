89dbb0bb072c47e6186c15e401f9b51ea7b6134d
rjp2525 <renophilibert@gmail.com>
2026-02-16 20:54:07 -0700

    filament uses livewire not inertia, exclude from inertia middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Symfony\Component\HttpFoundation\Response;
 
 class HandleInertiaRequests extends Middleware
 {
     protected $rootView = 'app';
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->is('admin') || $request->is('admin/*')) {
+            return $next($request);
+        }
+
+        return parent::handle($request, $next);
+    }
 
     public function version(Request $request): ?string
     {
